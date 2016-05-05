6dbae8c09a701eb919ee3dd106bb7d1314560966
changwentao <changwentao@changwentao.cn>
2016-05-05 15:25:54 +0800

    Modify plasma renderer.

--- plasma-android/app/src/main/java/cn/changwentao/plasma/PlasmaRenderer.java
+++ plasma-android/app/src/main/java/cn/changwentao/plasma/PlasmaRenderer.java
@@ -15,10 +15,16 @@ import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glUniform1f;
+import static android.opengl.GLES20.glGetUniformLocation;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetError;
+import static android.opengl.GLES20.GL_NO_ERROR;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -32,22 +38,21 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class PlasmaRenderer implements GLSurfaceView.Renderer {
     private static final String TAG = "PlasmaRenderer";
-    private static final int[] sColors = {0xFF7C8489, 0xFF4FB3A4, 0xFFFF7073, 0xFFF5B977, 0xFFFDFC7F};
+    private static final int[] sColors = {0xFF5FD9CD, 0xFFEAF786, 0xFFFFB5A1, 0xFFB8FFB8, 0xFFB8F4FF};
 
     private static final int BYTES_PER_FLOAT = 4;
     private static final String A_POSITION = "a_Position";
-    private static final String A_COLOR = "a_Color";
     private static final int POSITION_COMPONENT_COUNT = 2;
-    private static final int STRIDE = POSITION_COMPONENT_COUNT * BYTES_PER_FLOAT;
-
-    private final FloatBuffer vertexData;
+    private static final int STRIDE = 4 * BYTES_PER_FLOAT;
 
     private final Context mContext;
     private final int mVelocity;
 
+    private final FloatBuffer vertexData;
     private int program;
     private int aPositionLocation;
-    private int aColorLocation;
+    private int aTextureLocation;
+    private int uTimeLocation;
 
     private long globalStartTime;
 
@@ -56,12 +61,12 @@ public class PlasmaRenderer implements GLSurfaceView.Renderer {
         mVelocity = mContext.getResources().getInteger(R.integer.plasma_velocity);
 
         float[] tableVertices = {
-                0f, 0f,
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
-                -0.5f, -0.5f
+                0.0f, 0.0f, 0.5f, 0.5f,
+                -1.0f, -1.0f, 0.0f, 1.0f,
+                1.0f, -1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f, 0.0f,
+                -1.0f, 1.0f, 0.0f, 0.0f,
+                -1.0f, -1.0f, 0.0f, 1.0f
         };
 
         vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer();
@@ -82,14 +87,38 @@ public class PlasmaRenderer implements GLSurfaceView.Renderer {
         program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
         glUseProgram(program);
 
+
+
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
-        aColorLocation = glGetAttribLocation(program, A_COLOR);
+        aTextureLocation = glGetAttribLocation(program, "a_TextureCoord");
+
+        uTimeLocation = glGetUniformLocation(program, "uTime");
+        checkGlError("glGetUniformLocation uTime");
+        if (uTimeLocation == -1) {
+            throw new RuntimeException("Could not get attrib location for uTime");
+        }
 
         vertexData.position(0);
         glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
                 false, STRIDE, vertexData);
-
         glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(2);
+        glVertexAttribPointer(aTextureLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false,
+                STRIDE, vertexData);
+        glEnableVertexAttribArray(aTextureLocation);
+
+
+
+
+    }
+
+    private void checkGlError(String op) {
+        int error;
+        while ((error = glGetError()) != GL_NO_ERROR) {
+            Log.e(TAG, op + ": glError " + error);
+            throw new RuntimeException(op + ": glError " + error);
+        }
     }
 
     @Override
@@ -105,8 +134,46 @@ public class PlasmaRenderer implements GLSurfaceView.Renderer {
         float currentTime = (System.nanoTime() - globalStartTime) / 1000000000f;
 
         glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+        glUniform1f(uTimeLocation, currentTime);
     }
 
-    String vertexShaderSource = "";
-    String fragmentShaderSource = "";
+    String vertexShaderSource =
+            "attribute vec4 a_Position;" +
+                    "attribute vec2 a_TextureCoord;" +
+                    "varying vec2 v_TextureCoord;" +
+                    "void main()" +
+                    "{" +
+                    "    gl_Position = a_Position;" +
+                    "    v_TextureCoord = a_TextureCoord;\n" +
+                    "}";
+
+    String fragmentShaderSource =
+            "precision mediump float;" +
+                    "uniform float uTime;" +
+                    "varying vec2 v_TextureCoord;" +
+                    "void main()" +
+                    "{" +
+                    "    vec2 ca = vec2(0.1, 0.2);" +
+                    "    vec2 cb = vec2(0.7, 0.9);" +
+                    "    float da = distance(v_TextureCoord, ca);" +
+                    "    float db = distance(v_TextureCoord, cb);" +
+                    "    float t = uTime * 0.3;" +
+                    "    float c1 = sin(da * cos(t) * 16.0 + t * 4.0);" +
+                    "    float c2 = cos(v_TextureCoord.y * 8.0 + t);" +
+                    "    float c3 = cos(db * 14.0) + sin(t);" +
+                    "    float p = (c1 + c2 + c3) / 3.0;" +
+                    "    vec4 fColor;" +
+                    "    if(p < 0.2) {" +
+                    "        fColor = vec4("+ Color.red(sColors[0])/255.0f+", "+ Color.green(sColors[0])/255.0f+", "+ Color.blue(sColors[0])/255.0f+", 1.0);" +
+                    "    } else if(p < 0.4) {" +
+                    "        fColor = vec4("+ Color.red(sColors[1])/255.0f+", "+ Color.green(sColors[1])/255.0f+", "+ Color.blue(sColors[1])/255.0f+", 1.0);" +
+                    "    } else if(p < 0.6) {" +
+                    "        fColor = vec4("+ Color.red(sColors[2])/255.0f+", "+ Color.green(sColors[2])/255.0f+", "+ Color.blue(sColors[2])/255.0f+", 1.0);" +
+                    "    } else if(p < 0.8) {" +
+                    "        fColor = vec4("+ Color.red(sColors[3])/255.0f+", "+ Color.green(sColors[3])/255.0f+", "+ Color.blue(sColors[3])/255.0f+", 1.0);" +
+                    "    } else {" +
+                    "        fColor = vec4("+ Color.red(sColors[4])/255.0f+", "+ Color.green(sColors[4])/255.0f+", "+ Color.blue(sColors[4])/255.0f+", 1.0);" +
+                    "    }" +
+                    "    gl_FragColor = fColor;" +
+                    "}";
 }
